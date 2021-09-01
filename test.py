9a67e89695e8789492f54f40e6ac967b0559dc45
Jerred Wernke <jerredwernke@Jerreds-MacBook-Pro.local>
2021-09-01 17:47:06 -0400

    fist comment

---
+++ test.py
@@ -0,0 +1,17 @@
+
+def main():
+    weeklyHour = int(input("How many hours did you work: "))
+    ratePay = float(input("Your rate of Pay: "))
+    overTime = (weeklyHour - 40)
+    if (overTime > 0):
+        overTimePay = (overTime * (ratePay * 1.5))
+    grossPay = ((weeklyHour * ratePay) + overTimePay)  
+    if (grossPay > 401):
+        netPay = (grossPay - ((grossPay - 400) *.05))
+        print(f'Your netpay is under 400 {netPay}')
+        if (netPay > 1000):
+            netPay = (netPay - ((netPay - 1000) * .10))
+            print(f'Your netpay is over 1000 {netPay}')
+
+if __name__ == "__main__":
+    main()
